2d80b83f5c7d0c9b757392c1082244fb62c55910
이대현 <dleogus@danawa.com>
2022-02-28 17:59:27 +0900

    React User 튜토리얼
    - useState 훅 추가
    - 유효성 검사 추가

--- react-user-tutorial/src/components/Users/AddUser.js
+++ react-user-tutorial/src/components/Users/AddUser.js
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
+  const [enteredUserName, setEnteredUserName] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+  const [isValid, setIsValid] = useState(true);
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    
+    if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
+
+    console.log(enteredUserName, enteredAge);
+    setEnteredUserName('');
+    setEnteredAge('');
   };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUserName(event.target.value);
+  }
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  }
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor='username'>Username</label>
-        <input id='username' type='text'/>
+        <input 
+          id='username' 
+          type='text' 
+          value={enteredUserName} 
+          onChange={usernameChangeHandler}  
+        />
         <label htmlFor='age'>Age (Years)</label>
-        <input id='age' type='number' />
+        <input 
+          id='age'
+          type='number'
+          value={enteredAge} 
+          onChange={ageChangeHandler} 
+        />
         <Button type='submit'>Add User</Button>
       </form>
     </Card>

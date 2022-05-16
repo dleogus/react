b151dfad03f48319f6a13bede1843eed9e668fd8
이대현 <dleogus@danawa.com>
2022-05-16 17:11:07 +0900

    React form
    BasicForm 유효성 검증 적용

--- react-form/src/components/BasicForm.js
+++ react-form/src/components/BasicForm.js
@@ -1,22 +1,127 @@
+import { useState } from 'react';
+
 const BasicForm = (props) => {
+  const [enteredFirstName, setEnteredFirstName] = useState('');
+  const [enteredFirstNameIsTouched, setEnterFirstNameIsTouched] = useState(false);
+
+  const [enteredLastName, setEnteredLastName] = useState('');
+  const [enteredLastNameIsTouched, setEnteredLastNameIsTouched] = useState(false);
+
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredEmailIsTouched, setEnteredEmailIsTouched] = useState(false);
+
+  const enteredFirstNameIsValid = enteredFirstName.trim() !== '';
+  const firstNameInputIsInvalid = !enteredFirstNameIsValid && enteredFirstNameIsTouched;
+
+  const enteredLastNameIsValid = enteredLastName.trim() !== '';
+  const lastNameInputIsInvalid = !enteredLastNameIsValid && enteredLastNameIsTouched;
+
+  const enteredEmailisValid = enteredEmail.includes('@');
+  const emailInputIsInvalid = !enteredEmailisValid && enteredEmailIsTouched;
+
+  let formIsValid = false;
+
+  if (enteredFirstNameIsValid && enteredLastNameIsValid && enteredEmailisValid) {
+    formIsValid= true;
+  };
+  
+  const firstNameChangeHandler = (event) => {
+    setEnteredFirstName(event.target.value);
+  };
+
+  const firstNameBlurChangeHandler = (event) => {
+    setEnterFirstNameIsTouched(true);
+  };
+
+  const lastNameChangeHandler = (event) => {
+    setEnteredLastName(event.target.value);
+  };
+
+  const lastNameBlurChangeHandler = (event) => {
+    setEnteredLastNameIsTouched(true);
+  };
+
+  const emailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const emailBlurHandler = (event) => {
+    setEnteredEmailIsTouched(true);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    setEnterFirstNameIsTouched(true);
+    setEnteredLastNameIsTouched(true);
+    setEnteredEmailIsTouched(true);
+
+    if (!enteredFirstNameIsValid && !enteredLastNameIsValid && !enteredEmailisValid) {
+      return;
+    }
+    
+    console.log(enteredFirstName);
+    console.log(enteredLastName);
+    console.log(enteredEmail);
+
+    setEnteredFirstName('');
+    setEnteredLastName('');
+    setEnteredEmail('');
+    setEnterFirstNameIsTouched(false);
+    setEnteredLastNameIsTouched(false);
+    setEnteredEmailIsTouched(false);
+  }
+
+  const firstNameClasses = firstNameInputIsInvalid 
+    ? 'form-control invalid'
+    : 'form-control';
+
+  const lastNameClasses = lastNameInputIsInvalid 
+    ? 'form-control invalid'
+    : 'form-control';
+
+  const emailInputClasses = emailInputIsInvalid
+    ? 'form-control invalid'
+    : 'form-control';
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='control-group'>
-        <div className='form-control'>
+        <div className={firstNameClasses}>
           <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+          <input 
+            type='text' 
+            id='name'
+            onChange={firstNameChangeHandler}
+            onBlur={firstNameBlurChangeHandler}
+            value={enteredFirstName}
+           />
+          {firstNameInputIsInvalid && <p className='error-text'>FirstName must not be empty.</p>}
         </div>
-        <div className='form-control'>
+        <div className={lastNameClasses}>
           <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+          <input 
+            type='text'
+            id='name'
+            onChange={lastNameChangeHandler}
+            onBlur={lastNameBlurChangeHandler}
+            value={enteredLastName}
+          />
+          {lastNameInputIsInvalid && <p className='error-text'>LastName must not be empty</p>}
         </div>
       </div>
-      <div className='form-control'>
+      <div className={emailInputClasses}>
         <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+        <input 
+          type='text'
+          id='name' 
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+          value={enteredEmail}
+        />
       </div>
       <div className='form-actions'>
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );

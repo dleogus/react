1855ef428710603b1c921eed9c0f039938073e40
이대현 <dleogus@danawa.com>
2022-07-26 17:34:54 +0900

    React auth
    사용자 리디렉션

--- react-auth/src/components/Profile/ProfileForm.js
+++ react-auth/src/components/Profile/ProfileForm.js
@@ -1,9 +1,12 @@
 import { useRef, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import AuthContext from '../../store/auth-context';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+  const history = useHistory();
+
   const newPasswordInputRef = useRef();
   const authCtx = useContext(AuthContext);
 
@@ -24,7 +27,8 @@ const ProfileForm = () => {
       }
     }).then((res) => {
       // assumption: Always succeeds!
-
+      
+      history.replace('/');
     });
   };
 

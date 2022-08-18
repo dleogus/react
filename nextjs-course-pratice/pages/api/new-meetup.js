cb061d508f504cc5bea9a5848053b96edc3d1ce5
이대현 <dleogus@danawa.com>
2022-08-18 17:20:23 +0900

    Next.js
    몽고DB로 작업하기

--- nextjs-course-pratice/pages/api/new-meetup.js
+++ nextjs-course-pratice/pages/api/new-meetup.js
@@ -1,11 +1,26 @@
+import { MongoClient } from 'mongodb';
+
 // /api/new-meetup
 // POST /api/new-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
+    
+    const client = await MongoClient.connect(
+      'mongodb+srv://test:oxfzNotKSjRyRvGR@cluster0.jdzzl.mongodb.net/meetups?retryWrites=true&w=majority'
+    );
+    const db = client.db();
 
-    const { title, image, address, description } = data;
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
   }
 }
 
